d58402ec1f15f0398dfe2ce87419ea8720967934
aninha998765 <130669141+aninha998765@users.noreply.github.com>
2024-09-20 16:19:34 -0300

    commit zinatto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import { View, Pressable, Text } from 'react-native';
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+export function Header() {
+  return (
+    <View>
+      <View className="w-full flex flex-row items-center justify-between">
+        <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+          {/* Ícone de usuário */}
+          <Feather name="user" size={20} color="#121212" />
+        </Pressable>
+
+        <View className="flex flex-row items-center justify-center">
+          <Text className="text-lg font-bold" style={{ color: '#226752' }}>Care </Text>
+          <Text className="text-lg font-bold" style={{ color: '#121212' }}>Map</Text>
+        </View>
+
+        <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+          <Feather name="bell" size={20} color="#121212" />
+        </Pressable>
+      </View>
+
+      {/* Linha separadora */}
+      <View style={{ height: 0.2, backgroundColor: '#000000', marginTop: 10 }} />
+    </View>
+  );
+}
